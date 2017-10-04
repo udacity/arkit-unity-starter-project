98e04d052c3d107f02bcd32bc2e9846a9a838863
Vasanth Mohan <vmohan@vasanth-mohan.local>
2017-10-03 20:44:06 -0700

    Seamless Portal.

--- Assets/ARKitPortals/TransitionScripts/PortalTransition.cs
+++ Assets/ARKitPortals/TransitionScripts/PortalTransition.cs
@@ -12,7 +12,7 @@ public class PortalTransition : MonoBehaviour {
 	// The main camera is surrounded by a SphereCollider with IsTrigger set to On
 	void OnTriggerEnter(Collider portal){
 		Portal logic = portal.GetComponentInParent<Portal> ();
-		transform.position = logic.PortalCameras[1].transform.position;
+		transform.position = logic.PortalCameras[1].transform.position - GetComponentInChildren<Camera>().transform.localPosition;
 
 		if (OnPortalTransition != null) {
 			// Emit a static OnPortalTransition event every time the camera enters a portal. The DoorManager listens for this event.
